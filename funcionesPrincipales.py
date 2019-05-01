7d662a9efb98bb4d7052202350fa860d494ccf5c
KingNBear <sreynoso0499@gmail.com>
2019-04-30 22:00:13 -0500

    asd

---
+++ funcionesPrincipales.py
@@ -0,0 +1,48 @@
+
+
+import csv
+from math import sin, cos, sqrt, atan2, radians
+
+def Obtenerdata(data):
+    with open('DatosReales.csv','r') as registros:
+        lector = csv.reader(registros,delimiter=',')
+        for fila in lector:
+            CODCP = int(fila[0]) #Codigo de centro poblado (unico)
+            DEP = fila[1]   #Departamento
+            PROV = fila[2] #Provincia
+            DIST = fila[3] #Distrito
+            NOMCP = fila[4] #Nombre del centro poblado
+            XCP = float(fila[5]) #Posicion X
+            YCP = float(fila[6]) #Posicion Y
+            data.append((CODCP, DEP, PROV, DIST, NOMCP, XCP, YCP))
+
+codcp = 0
+dep  = 1
+prov = 2
+dist = 3
+nomcp = 4
+xcp = 5
+ycp = 6
+
+def calcularDistancia(x1,y1,x2,y2):        
+    R = 6373.0  # approximate radius of earth in km
+    lat1 = radians(y1) #y1
+    lon1 = radians(x1) #x1
+    lat2 = radians(y2)  #y2
+    lon2 = radians(x2) #x2  
+    dlon = lon2 - lon1 #Distancia entre longitudes
+    dlat = lat2 - lat1 #Distancia entre latitudes   
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2 
+    c = 2 * atan2(sqrt(a), sqrt(1 - a)) 
+    distance = R * c
+    return distance
+
+data =[]
+Obtenerdata(data) #dsda
+
+
+print(data[0][xcp],data[0][ycp])
+print(data[1][xcp],data[1][ycp])
+print(calcularDistancia(data[0][xcp],data[0][ycp],data[1][xcp],data[1][ycp]))
+
+#abacho
